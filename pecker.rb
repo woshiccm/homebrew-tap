802c4f8f8822959021b8fc8d498fd334170015b5
Roy Cao <caoming.roy@bytedance.com>
2020-07-08 14:46:09 +0800

    Bump Pecker version to 0.4.0

--- pecker.rb
+++ pecker.rb
@@ -2,7 +2,7 @@ class Pecker < Formula
   desc "A tool to detect unused Swift code"
   homepage "https://github.com/woshiccm/Pecker"
   url "https://github.com/woshiccm/Pecker.git",
-      :tag => "0.3.0"
+      :tag => "0.4.0"
   head "https://github.com/woshiccm/Pecker.git"
 
   depends_on :xcode => ["10.0", :build]
